a4953a4daf3512fd16a14b722b9369c43c1cf491
Jingz <455786463@qq.com>
2015-07-24 21:51:03 +0800

    offer a instance to visit Factories and manager elem's producing and resycle;when add a new MoveAbleElem type,just need to call function:registerElemAndFactory.PS:if feel have to modify these codes when add a new type,if must be something wrong on the usage

---
+++ Classes/MoveAbleElemManager.cpp
@@ -0,0 +1,100 @@
+#include "MoveAbleElemManager.h"
+#include "cocos2d.h"
+#include "MoveAbleElemFactory.h"
+#include "MoveAbleElem.h"
+
+#include "Obstacle.h"
+#include "Coin.h"
+#include "Player.h"
+
+
+using namespace std;
+USING_NS_CC;
+
+
+MoveAbleElemManager * MoveAbleElemManager::s_pMoveAbleElemManager = nullptr;
+
+MoveAbleElemManager::MoveAbleElemManager() :_mElemFactories({})
+{
+}
+
+MoveAbleElemManager::~MoveAbleElemManager()
+{
+}
+
+MoveAbleElemManager * MoveAbleElemManager::getInstance(void)
+{
+	if (s_pMoveAbleElemManager == nullptr)
+	{
+		s_pMoveAbleElemManager = new MoveAbleElemManager();
+		s_pMoveAbleElemManager->init();
+	}
+	return s_pMoveAbleElemManager;
+}
+
+void MoveAbleElemManager::destroyInstance(void)
+{
+	if (s_pMoveAbleElemManager != nullptr)
+	{
+		s_pMoveAbleElemManager->release();
+		s_pMoveAbleElemManager = nullptr;
+	}
+	
+}
+
+void MoveAbleElemManager::init(void)
+{
+	//todo here should try load something
+}
+
+void MoveAbleElemManager::recycleElem(MoveAbleElem * pMoveAbleElem)
+{
+	CCASSERT(_mElemFactories.at(pMoveAbleElem->getElemType()), "MoveAbleElemFactory don't include this types");
+	_mElemFactories.at(pMoveAbleElem->getElemType())->recycleElem(pMoveAbleElem);
+}
+
+MoveAbleElem * MoveAbleElemManager::GenerateOneElem(unsigned short iTypeId) throw (std::out_of_range)
+{
+	try
+	{
+
+		if (_mElemFactories.at(iTypeId) == nullptr)
+		{
+			try
+			{
+				auto pFactory = _mFactoryCreatFunc.at(iTypeId)();
+
+				_mElemFactories.insert(iTypeId, pFactory);
+				return pFactory->getMoveAbleElem();
+			}
+			catch (std::out_of_range e)
+			{
+				CCLOG("MoveAbleElem Type:%ud 's factory creatFunc is null", iTypeId);
+				throw e;
+			}
+
+		}
+
+		return _mElemFactories.at(iTypeId)->getMoveAbleElem();
+	}
+	catch (std::out_of_range e)
+	{
+		try
+		{
+			auto pFactory = _mFactoryCreatFunc.at(iTypeId)();
+			_mElemFactories.insert(iTypeId, pFactory);
+			return pFactory->getMoveAbleElem();
+		}
+		catch (std::out_of_range e)
+		{
+			CCLOG("You should call registerSelf(void) in you new MoveAbleElem's init function", iTypeId);
+			throw e;
+		}
+	}
+
+}
+
+void MoveAbleElemManager::registerElemAndFactory(unsigned short iTypeId, FactoryCreateFunc createFunc)
+{
+	_mFactoryCreatFunc.insert(make_pair(iTypeId, createFunc));
+}
---
+++ Classes/MoveAbleElemManager.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "cocos2d.h"
+#include <map>
+#include <functional>
+#include <exception>
+#include "MoveAbleElemFactory.h"
+
+class MoveAbleElem;
+
+
+class MoveAbleElemManager:public cocos2d::Ref
+{
+	typedef std::function<MoveAbleElemBaseFactory*(void)> FactoryCreateFunc;
+	friend class GameLayer;
+	MoveAbleElemManager();
+public:
+	
+	~MoveAbleElemManager();
+
+	static MoveAbleElemManager * getInstance(void);
+
+	static void destroyInstance(void);
+
+	void init(void);
+
+	void recycleElem(MoveAbleElem * pMoveAbleElem);
+
+	//jingz 
+	// !!!! take care,if caused a out_of_range exception
+	MoveAbleElem * GenerateOneElem(unsigned short iTypeId) throw (std::out_of_range);
+
+	void registerElemAndFactory(unsigned short iTypeId, FactoryCreateFunc createFunc);
+
+private:
+	static MoveAbleElemManager * s_pMoveAbleElemManager;
+	
+	cocos2d::Map<unsigned short, MoveAbleElemBaseFactory * > _mElemFactories;
+	std::map<unsigned short, FactoryCreateFunc> _mFactoryCreatFunc;
+};
