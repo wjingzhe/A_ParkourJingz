a0f81511f4969bbda5a77b2296272a82067c4b2f
Jingz <455786463@qq.com>
2015-07-27 15:28:04 +0800

    Debug:block because i used the wrong container in muti_thread loading;Improment:create all the elems in another thread

--- Classes/MoveAbleElemFactory.h
+++ Classes/MoveAbleElemFactory.h
@@ -10,12 +10,13 @@
 static std::atomic_flag s_lock = ATOMIC_FLAG_INIT;
 
 #define INCREASEMENT 5
-#define PRE_CREATE_INCREASEMENT 30
+#define PRE_CREATE_INCREASEMENT 15
+#define PRE_CREATE_BASE 30
 
-#define ADD_MOVEABLE_ELEM(SIZE) \
+#define ADD_MOVEABLE_ELEM(CONTAINER,SIZE) \
 		for (size_t i = 0; i < SIZE; ++i)\
 		{\
-			_vUsedElem.push_back(MoveAbleElem::create()); \
+			CONTAINER.push_back(MoveAbleElem::create()); \
 		}
 
 template<class MoveAbleElem>
@@ -28,16 +29,16 @@ protected:
 		_pPreGenElem.clear();
 
 		auto & _lock = s_lock;
-		auto & _vUsedElem = this->_vUsedElem;
+		auto & _pPreGenElem = this->_vReadyElem;
 		//新起线程去完成加载，用锁来控制唯一性
 		_theadPreload = std::async(
-			[&_lock, &_vUsedElem]()->void
+			[&_lock, &_pPreGenElem]()->void
 		{
 			while (_lock.test_and_set(std::memory_order_acquire))
 			{
 				//尝试取锁
 			}
-			ADD_MOVEABLE_ELEM(PRE_CREATE_INCREASEMENT);
+			ADD_MOVEABLE_ELEM(_pPreGenElem, PRE_CREATE_BASE);
 			_lock.clear();
 			//解锁
 		}
@@ -92,22 +93,42 @@ public:
 			}
 			for each (auto pMoveAbleElem in _pPreGenElem)
 			{
-				_vUsedElem.push_back(pMoveAbleElem);
+				_vReadyElem.push_back(pMoveAbleElem);
 			}
 			_pPreGenElem.clear();
 			s_lock.clear();
 		}
 
-		if (_vUsedElem.size() <= 0)
+		if (_vReadyElem.size() <= 0)
 		{
-			ADD_MOVEABLE_ELEM(INCREASEMENT);
+			ADD_MOVEABLE_ELEM(_vReadyElem, INCREASEMENT);
 		}
 
 		//todo 
-		MoveAbleElemPtr pElem = dynamic_cast<MoveAbleElemPtr >(_vUsedElem.back());//不用auto 是为了确认返回非const MoveAbleElem *
-		_vReadyElem.push_back(pElem);
+		MoveAbleElemPtr pElem = dynamic_cast<MoveAbleElemPtr >(_vReadyElem.back());//不用auto 是为了确认返回非const MoveAbleElem *
+		_vUsedElem.push_back(pElem);
 		pElem->setUsed();
-		_vUsedElem.pop_back();
+		_vReadyElem.pop_back();
+
+		if (_vReadyElem.size()<=1)
+		{
+			auto & _lock = s_lock;
+			auto & _pPreGenElem = this->_pPreGenElem;
+			//新起线程去完成加载，用锁来控制唯一性
+			_theadPreload = std::async(
+				[&_lock, &_pPreGenElem]()->void
+				{
+					while (_lock.test_and_set(std::memory_order_acquire))
+					{
+						//尝试取锁
+					}
+					ADD_MOVEABLE_ELEM(_pPreGenElem, PRE_CREATE_INCREASEMENT);
+					_lock.clear();
+					//解锁
+				}
+			);
+		}
+
 		return pElem;
 	}
 
