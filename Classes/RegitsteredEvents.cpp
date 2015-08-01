bd626b88cc22c37c9b9e027a168d2043c5d8f0e2
Jingz <455786463@qq.com>
2015-08-01 19:25:28 +0800

    add GAME_START/GAME_OVER/GAME_RESTART/SHOW_RESTART as event name

--- Classes/RegitsteredEvents.cpp
+++ Classes/RegitsteredEvents.cpp
@@ -2,3 +2,7 @@
 
 const std::string RegitsteredEvents::GOLD_CHANGED = "GOLD_CHANGED";
 const std::string RegitsteredEvents::SHOW_RESTART = "SHOW_RESTART";
+const std::string RegitsteredEvents::GAME_OVER = "GAME_OVER";
+const std::string RegitsteredEvents::GAME_START = "GAME_START";
+const std::string RegitsteredEvents::GAME_RESTART = "GAME_RESTART";
+
--- Classes/RegitsteredEvents.h
+++ Classes/RegitsteredEvents.h
@@ -2,13 +2,13 @@
 #include <string>
 #include <vector>
 
-//todo 如何通过放射注册消息事件？？
+//todo 如何通过反射注册消息事件？？
 class RegitsteredEvents
 {
 public:
 	static const std::string GOLD_CHANGED;
 	static const std::string SHOW_RESTART;
-
-private:
-	std::vector<std::string> _vRegisteredEvents;
+	static const std::string GAME_OVER;
+	static const std::string GAME_START;
+	static const std::string GAME_RESTART;
 };
